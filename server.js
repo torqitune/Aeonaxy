d2eacc8e016fcb8c984d41b1e62c8ad8da3ec2a7
Aaryan <aaryan.aa2905@gmail.com>
2024-04-15 22:24:52 +0530

    shifted connection string of DB to .env

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.set("view engine", "ejs");
 
 //conecting to our mongoose DB
 mongoose
-  .connect("mongodb://127.0.0.1:27017/aenoaxy")
+  .connect(process.env.DATABASE)
   .then(() => console.log("mongoDB connected"))
   .catch((err) => console.log(err));
 
